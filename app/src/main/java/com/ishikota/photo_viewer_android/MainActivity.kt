5454da6b2e0882ddb023771de92655395272386a
kota-ishimoto <kota-ishimoto@cookpad.com>
2019-12-20 12:09:24 +0900

    Init project

---
+++ app/src/main/java/com/ishikota/photo_viewer_android/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ishikota.photo_viewer_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
