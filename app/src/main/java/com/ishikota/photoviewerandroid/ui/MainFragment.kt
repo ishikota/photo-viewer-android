562e6a0283ae64b890a92e6186523046fda09c01
kota-ishimoto <kota-ishimoto@cookpad.com>
2019-12-24 13:42:29 +0900

    Delete sample code

--- app/src/main/java/com/ishikota/photoviewerandroid/ui/MainFragment.kt
+++ app/src/main/java/com/ishikota/photoviewerandroid/ui/MainFragment.kt
@@ -5,9 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import com.ishikota.photoviewerandroid.R
-import kotlinx.android.synthetic.main.main_fragment.*
 
 class MainFragment : Fragment() {
 
@@ -17,13 +15,5 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.main_fragment, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        text.setOnClickListener {
-            val action = MainFragmentDirections.actionMainFragmentToSubFragment(101)
-            findNavController().navigate(action)
-        }
     }
 }
