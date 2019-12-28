70f4dfa716a64fd4ec3677081f1b24315c83ac3d
kota-ishimoto <kota-ishimoto@cookpad.com>
2019-12-28 17:20:53 +0900

    Delete unused

--- app/src/main/java/com/ishikota/photoviewerandroid/ui/photolist/PhotoListPageKeyedDataSource.kt
+++ app/src/main/java/com/ishikota/photoviewerandroid/ui/photolist/PhotoListPageKeyedDataSource.kt
@@ -4,13 +4,10 @@ import androidx.lifecycle.MutableLiveData
 import androidx.paging.DataSource
 import com.ishikota.photoviewerandroid.data.api.entities.Photo
 import com.ishikota.photoviewerandroid.data.repository.PhotoRepository
-import com.ishikota.photoviewerandroid.infra.moshi.buildDefaultMoshi
 import com.ishikota.photoviewerandroid.infra.paging.NetworkStatePageKeyedDataSource
-import com.squareup.moshi.Types
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
-import timber.log.Timber
 
 class PhotoListPageKeyedDataSource(
     private val listOrder: PhotoRepository.Order,
@@ -30,19 +27,12 @@ class PhotoListPageKeyedDataSource(
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(
                 { items ->
-                    val s = modelsToJson(items)
-                    Timber.d(s)
                     successCallback(items)
                 },
                 { throwable -> failureCallback(throwable) }
             )
             .also { compositeDisposable.add(it) }
     }
-
-    inline fun<reified T> jsonToModels(json: String): List<T>? =
-        buildDefaultMoshi().adapter<List<T>>(Types.newParameterizedType(List::class.java, T::class.java)).fromJson(json)
-    inline fun <reified T> modelsToJson(models: List<T>): String =
-        buildDefaultMoshi().adapter<List<T>>(Types.newParameterizedType(List::class.java, T::class.java)).toJson(models)
 
     override fun getNextPageKey(current: Int?): Int = if (current == null) 1 else current + 1
 
