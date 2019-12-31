1d3beddf20545074bafe1fa68a74aafb6b68cc60
kota-ishimoto <kota-ishimoto@cookpad.com>
2019-12-31 09:49:43 +0900

    Add search menu

--- app/src/main/java/com/ishikota/photoviewerandroid/ui/MainActivity.kt
+++ app/src/main/java/com/ishikota/photoviewerandroid/ui/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.ishikota.photoviewerandroid.ui
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
@@ -18,5 +21,20 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
         binding.toolbar.setupWithNavController(findNavController(R.id.nav_host_fragment))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (item.itemId == R.id.search) {
+            Toast.makeText(this, "TODO search", Toast.LENGTH_SHORT).show()
+            true
+        } else {
+            super.onOptionsItemSelected(item)
+        }
+
     }
 }
