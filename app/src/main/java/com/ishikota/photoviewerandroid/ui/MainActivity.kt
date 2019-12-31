8b45831464f99f5f8e7b6b141f5356bc5c273e5b
kota-ishimoto <kota-ishimoto@cookpad.com>
2019-12-31 09:27:28 +0900

    Add toolbar

--- app/src/main/java/com/ishikota/photoviewerandroid/ui/MainActivity.kt
+++ app/src/main/java/com/ishikota/photoviewerandroid/ui/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.ishikota.photoviewerandroid.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.ishikota.photoviewerandroid.R
 import com.ishikota.photoviewerandroid.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: MainActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        DataBindingUtil.setContentView<MainActivityBinding>(this, R.layout.main_activity)
+        binding = DataBindingUtil.setContentView(this, R.layout.main_activity)
+
+        setSupportActionBar(binding.toolbar)
+        binding.toolbar.setupWithNavController(findNavController(R.id.nav_host_fragment))
     }
 }
