fba45c24ebce4a886635b3787cb5524f1d3e846f
kota-ishimoto <kota-ishimoto@cookpad.com>
2020-01-24 14:26:05 +0900

    Move toolbar to fragment

--- app/src/main/java/com/ishikota/photoviewerandroid/ui/MainActivity.kt
+++ app/src/main/java/com/ishikota/photoviewerandroid/ui/MainActivity.kt
@@ -1,31 +1,13 @@
 package com.ishikota.photoviewerandroid.ui
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
-import androidx.navigation.findNavController
-import androidx.navigation.ui.setupWithNavController
 import com.ishikota.photoviewerandroid.R
-import com.ishikota.photoviewerandroid.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: MainActivityBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.main_activity)
-
-        setSupportActionBar(binding.toolbar)
-        supportActionBar?.setDisplayShowTitleEnabled(false)
-        binding.toolbar.setupWithNavController(findNavController(R.id.nav_host_fragment))
-
-        binding.menuAccount.setOnClickListener {
-            Toast.makeText(this, "TODO account", Toast.LENGTH_SHORT).show()
-        }
-        binding.menuSearch.setOnClickListener {
-            Toast.makeText(this, "TODO search", Toast.LENGTH_SHORT).show()
-        }
+        setContentView(R.layout.main_activity)
     }
 }
