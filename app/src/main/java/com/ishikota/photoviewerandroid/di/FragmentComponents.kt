b657309c83cd4f3e093eed3345f0d5d99a18b882
kota-ishimoto <kota-ishimoto@cookpad.com>
2020-01-27 08:33:55 +0900

    Tweak

--- app/src/main/java/com/ishikota/photoviewerandroid/di/FragmentComponents.kt
+++ app/src/main/java/com/ishikota/photoviewerandroid/di/FragmentComponents.kt
@@ -5,6 +5,7 @@ import com.ishikota.photoviewerandroid.ui.collectionlist.CollectionListComponent
 import com.ishikota.photoviewerandroid.ui.photodetail.PhotoDetailComponent
 import com.ishikota.photoviewerandroid.ui.photolist.PhotoListComponent
 import com.ishikota.photoviewerandroid.ui.userdetail.UserDetailComponent
+import com.ishikota.photoviewerandroid.ui.userdetail.likedphotos.UserDetailLikedPhotosComponent
 import com.ishikota.photoviewerandroid.ui.userdetail.postedphotos.UserDetailPostedPhotosComponent
 import dagger.Module
 
@@ -14,6 +15,7 @@ import dagger.Module
     PhotoDetailComponent::class,
     CollectionDetailComponent::class,
     UserDetailComponent::class,
-    UserDetailPostedPhotosComponent::class
+    UserDetailPostedPhotosComponent::class,
+    UserDetailLikedPhotosComponent::class
 ])
 class FragmentComponents
